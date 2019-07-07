593754c9be18d6c4b2bf806219bc9bb0b64063f3
Ilya Grishkov <ilyagriskov99@icloud.com>
2019-07-07 18:23:28 +0200

    Working CI

--- tests/preprocessor/test_null_value_cleaner.py
+++ tests/preprocessor/test_null_value_cleaner.py
@@ -2,9 +2,5 @@ from preprocessor import null_value_cleaner
 
 
 def test_nnd():
-    assert null_value_cleaner.nnd() == ('remove', [2], []), 'Remove approach with no rows neither to keep nor remove'
+    assert null_value_cleaner.nnd() == ('remove', [], []), 'Remove approach with no rows neither to keep nor remove'
 
-
-# if __name__ == '__main__':
-#     no_parameter_nnd()
-#     print("Everything passed")
